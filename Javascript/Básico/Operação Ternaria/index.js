02ace5dc7de5716cb9ebd3030066e422ce7be054
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-08-04 23:38:44 -0300

    Ternary operation

---
+++ Javascript/Básico/Operação Ternaria/index.js
@@ -0,0 +1,8 @@
+//Se a condição for verdadeira será atribuida a informação após o "?"
+//Se a condição for Falsa será atribuida a informação após o ":"
+
+const pontos = 1000
+
+const usuario = pontos >= 1000 ? 'VIP' : 'Normal'
+
+console.log(usuario)
