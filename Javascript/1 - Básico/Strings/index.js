d05ffc52dae4991d809a634bdf99bc061eee4235
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-08-05 12:47:07 -0300

    Strings

---
+++ Javascript/1 - Básico/Strings/index.js
@@ -0,0 +1,15 @@
+// há 3 maneiras de se usar strings em Javascript
+
+//com 'Aspas Simples'
+
+console.log('String com aspas simples')
+
+// com aspas duplas
+
+console.log("String com aspas duplas")
+
+// e com interpolação, onde se pode usar elementos junto á ela
+
+const tipo = 'interpolação'
+
+console.log(`String com ${tipo}`)
