21fb99bbcac24e48ea35a8cd646058cc9da9f273
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-08-04 23:47:43 -0300

    Tipo de dados

---
+++ Javascript/Básico/Tipos de Dados/index.js
@@ -0,0 +1,17 @@
+// String representam valores de texto
+let fruta = "Laranja"
+
+// Boolean possuem valores de verdadeiros ou falso
+let falso = false
+let verdadeiro = true
+
+// Number representam valores decimais ou inteiros
+let soma = 45 + 55
+
+//Null: representa um valor nulo ou vazio
+let valor = null
+
+// Undefined é variável que foi declarada, mas que não foi atribuida um valor. 
+
+let velocidade;
+console.log(velocidade) // Como não tem valor será undefined (indefinido)
