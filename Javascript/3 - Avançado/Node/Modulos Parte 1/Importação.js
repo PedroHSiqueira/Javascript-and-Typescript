9c9e1c2db9b377b55cdd740e0dd1c4fa36acfe90
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-08-10 22:45:21 -0300

    Node

---
+++ Javascript/3 - Avançado/Node/Modulos Parte 1/Importação.js
@@ -0,0 +1,7 @@
+//importando elementos Via desestruturação 
+
+const { Pessoa } = require('./exportação')
+
+const pessoa1 = new Pessoa('Pedro')
+
+console.log(pessoa1)
---
+++ Javascript/3 - Avançado/Node/Modulos Parte 1/exportação.js
@@ -0,0 +1,10 @@
+//exportando elementos
+//pode ser escrito sem o 'module' na frente
+
+class Pessoa {
+  constructor(nome){
+    this.nome = nome
+  }
+}
+
+exports.Pessoa = Pessoa
