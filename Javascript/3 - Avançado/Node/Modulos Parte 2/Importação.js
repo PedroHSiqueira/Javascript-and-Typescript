2e6b2ef58d897ea42a1cc059dd7a88928974f98e
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-08-11 17:03:01 -0300

    Exportar Função

---
+++ Javascript/3 - Avançado/Node/Modulos Parte 2/Importação.js
@@ -0,0 +1,5 @@
+//importando elementos Via desestruturação 
+
+const multiplicacao = require('./exportação')
+
+console.log(multiplicacao(2,2))
---
+++ Javascript/3 - Avançado/Node/Modulos Parte 2/exportação.js
@@ -0,0 +1,6 @@
+//exportando elementos
+//pode ser escrito sem o 'module' na frente
+
+module.exports = function(x, y){
+  return x * y
+}
