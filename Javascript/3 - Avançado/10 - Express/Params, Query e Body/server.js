9f669b42ccc6f3eacf5236efb09f7074634adbf8
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-08-16 20:05:44 -0300

    Update

--- Javascript/3 - Avançado/10 - Express/Params, Query e Body/server.js
+++ Javascript/3 - Avançado/10 - Express/Params, Query e Body/server.js
@@ -1,37 +1,33 @@
 const express = require("express");
 const app = express();
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-//req => requisição
-//res => resposta
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
   <form action="/" method="POST">
-  Nome: <input type="text name="nome>
-  <button>enviar formulário</button>
+  Nome do cliente: <input type="text" name="qualquercoisa"><br>
+  Outro campo: <input type="text" name="aquioutrocampo">
+  <button>Olá mundo</button>
   </form>
-  `)
-})
+  `);
+});
 
-app.get("/testes/:idUsuarios?/:parametros?", (req, res) => {
-  //mostra os parametros da url
-  console.log(req.params)
+app.get("/testes/:idUsuarios?/:parametro?", (req, res) => {
+  // /profiles/3
+  // /profiles/?chave1=valor1&chave2=valor2&chave3=valor3
+  //
+  console.log(req.params);
+  console.log(req.query);
+  res.send(req.query.facebookprofile);
+});
 
-  //mostra query strings 
-  // ex: abc.com/?nome=Pedro
-
-  console.log(req.query)
-  res.send(req.query.nome)
-})
-
-app.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('recebi o formulário')
-})
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send(`O que você me enviou foi: ${req.body.qualquercoisa}`);
+});
 
 app.listen(3000, () => {
-  console.log('acesse http://localhost:3000')
-  console.log('Servidor executando na porta 3000')
-})
+  console.log("Acessar http://localhost:3000");
+  console.log("Servidor executando na porta 3000");
+});
